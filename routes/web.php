bfe73e69680b91ac7f7558a3993c9089ffe43763
unknown <sarah.sudan91@gmail.com>
2024-11-09 23:57:31 +0200

    items

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ItemController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -22,11 +23,17 @@ Route::get('/', function () {
 
 Route::get('/orders',[\App\Http\Controllers\PDFController::class,'orders']);
 
-
+//users
 Route::get('/users',[UserController::class,'create']);
 Route::get('/users/{userId}',[UserController::class,'destroy']);
 Route::post('/createUSer',[UserController::class,'store']);
 Route::post('/updateUser',[UserController::class,'update']);
 
-
+//export order to excel
 Route::get('/exportExcel',[OrderController::class,'exportExcel']);
+
+//items
+Route::get('/items',[ItemController::class,'index']);
+Route::get('/items/{itemId}',[ItemController::class,'destroy']);
+Route::post('/createItem',[ItemController::class,'store']);
+Route::post('/updateItem',[ItemController::class,'update']);
